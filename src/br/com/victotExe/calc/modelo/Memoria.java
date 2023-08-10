fc38fed08de5c852da6fe2cbb5407848e8cf1c07
vitot <victorfarian@gmail.com>
2023-08-09 23:17:06 -0300

    Implementando identificador de tipos de botoes

--- src/br/com/victotExe/calc/modelo/Memoria.java
+++ src/br/com/victotExe/calc/modelo/Memoria.java
@@ -5,36 +5,79 @@ import java.util.List;
 
 public class Memoria {
 
-	private static final Memoria instancia = new Memoria();
-	
-	private final List<MemoriaObservador> observadores = new ArrayList<>();
-	
-	private String textoAtual = "";
-	
-	//	singleTon(padrão que permite apenas uma instância da classe
-	private Memoria() {//método construtor privado
+	private enum TipoComando {
+		ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA, INVERTER, PORCENTAGEM
 	}
-	
+
+	private static final Memoria instancia = new Memoria();
+
+	private final List<MemoriaObservador> observadores = new ArrayList<>();
+
+	private String textoAtual = "";
+
+	// singleTon(padrão que permite apenas uma instância da classe
+	private Memoria() {// método construtor privado
+	}
+
 	public static Memoria getInstancia() {
 		return instancia;
 	}
-	
+
 	public void AdcionarObservador(MemoriaObservador observador) {
 		observadores.add(observador);
-		
+
 	}
 
 	public String getTextoAtual() {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
-	
-	public void processarComando(String valor) {
-		
-		if("AC".equals(valor)) {
+
+	public void processarComando(String texto) {
+
+		TipoComando tipoComando = detectarTipoComando(texto);
+
+		System.out.println(tipoComando);
+
+		if ("AC".equals(texto)) {
 			textoAtual = "";
 		} else {
-			textoAtual += valor;
+			textoAtual += texto;
 		}
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private TipoComando detectarTipoComando(String texto) {
+
+		if (textoAtual.isEmpty() && texto == "0") {
+			return null;
+		}
+
+		try {
+			Integer.parseInt(texto);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			// Quando não for numero...
+			if ("AC".equals(texto)) {
+				return TipoComando.ZERAR;
+			} else if ("/".equals(texto)) {
+				return TipoComando.DIV;
+			} else if ("*".equals(texto)) {
+				return TipoComando.MULT;
+			} else if ("-".equals(texto)) {
+				return TipoComando.SUB;
+			} else if ("+".equals(texto)) {
+				return TipoComando.SOMA;
+			} else if ("=".equals(texto)) {
+				return TipoComando.IGUAL;
+			} else if (",".equals(texto)) {
+				return TipoComando.VIRGULA;
+			}else if ("+/-".equals(texto)) {
+				return TipoComando.INVERTER;
+			}else if ("%".equals(texto)) {
+				return TipoComando.PORCENTAGEM;
+			}
+		}
+
+		return null;
 	}
 }
